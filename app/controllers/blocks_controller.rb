e2e97f155007a9310a7da280fc45114e4abd85b2
jovanni-dev <jovanni.millers123@outlook.com>
2023-02-13 04:10:55 -0500

    upgrade_blocks_controller

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -6,11 +6,7 @@ class BlocksController < ApplicationController
     @conversation = conversation
   end
 
-  def create
-    authorize conversation
-    conversation.touch(blocked_by_column)
-    redirect_to root_path, notice: t(".notice", other_recipient:)
-  end
+
 
   private
 
