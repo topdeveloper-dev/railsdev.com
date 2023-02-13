db34e9080069948b6bc95e3529a0a0e8c42dc90d
jovanni-dev <jovanni.millers123@outlook.com>
2023-02-13 18:47:15 -0500

    fix_applicaiton_docer

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
+module Railsdevs
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
